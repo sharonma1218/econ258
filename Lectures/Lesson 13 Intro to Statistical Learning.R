425f1ba0eafe16b0e16483fcdcc2d1063bc02b1f
Sharon Ma <119529795+sharonma1218@users.noreply.github.com>
2023-03-10 15:18:52 -0500

    Add files via upload

---
+++ Lectures/Lesson 13 Intro to Statistical Learning.R
@@ -0,0 +1,118 @@
+#------------------------------------------------------------- #
+# Lesson 13: Intro to Statistical Learning                     #
+#                                                              #      
+# An Exercise with Machine Learning Flavor                     #
+# Predicting using OLS versus K-Nearest Neighbor               # 
+#                                                              #
+#--------------------------------------------------------------# 
+
+#install.packages("class")
+library(class)
+
+### Part 1: Create "Fake" data based on a function y = x^3 + epsilon
+
+#
+# A. Create 1000 values of x such that: 
+#    the next value of x is equal to the previous value of x plus 0.02.
+# 
+
+x  = numeric(1000)
+x[1] = -10
+
+# Create code looping the values. 
+
+
+for (t in 2:1000) {
+  x[t] <- x[t-1] + 0.02
+}
+
+# B. Now let's create fake y that depends on x and the epsilon component. 
+
+eps <- rnorm(1000, 0, 100) 
+
+y <- 2525 + x^3 + eps 
+
+# Plot y on x
+
+plot(y ~ x, las = 1)
+
+
+# Put in your fake data in a data frame (y,x, and eps)
+
+
+fake_data <- data.frame( y= y,
+                         x = x,
+                         eps = eps)
+
+### Part 2: Sampling for training and test data
+
+set.seed(1234) # To make sure we all get the same answers.
+
+# I am going to randomly assign a number to the data
+fake_data$random_unif <- runif(nrow(fake_data))
+learning_sample       <- subset(fake_data, random_unif<=0.3) 
+test_sample           <- subset(fake_data, random_unif>0.7)
+
+# Other ways to get the train and test data
+# train           <- sample(1:nrow(fake_data), 500)
+# learning_sample <-  fake_data[-train,]
+# test_sample     <- fake_data[train,]
+
+
+# Plot the y on x for both the learning and test sample
+
+plot(y ~ x, data = learning_sample, las = 1)
+plot(y ~ x, data = test_sample, las = 1)
+
+
+
+### Part 3: Linear Regression and K-nearest neighbour
+
+
+# Compare a linear model (parametric) versus a k-nearest neighbour (non-parametric)
+
+######
+# A. Estimate the linear regression of y on x using the learning sample
+######
+
+ols_model <- lm(y ~ x, data = learning_sample)
+
+######
+# B. Predict y using the linear regression model you estimated before for the learning sample
+######
+
+learning_sample$predict_y <- predict(ols_model)
+
+######
+# C. Now predict the y using the linear regression model you estimated for the test sample  
+######
+
+test_sample$predict_y <- predict(ols_model, newdata = test_sample)
+
+######
+# D. Calculate the mean square errors for both prediction
+######
+
+MSE_ols_learn <- mean((learning_sample$y-learning_sample$predict_y)^2)
+MSE_ols_test <-  mean((test_sample$y-test_sample$predict_y)^2)
+
+######
+# E. Plot the y for both (in a separate graph is fine)
+######
+
+plot(predict_y ~ x, data = learning_sample, las = 1)
+plot(predict_y ~ x, data = test_sample, las = 1)
+
+######
+# F. The code below estimates a model using k nearest neighbour, and predicts y
+######
+
+kkn_predict     <- knn(train = data.frame(learning_sample$x), test = data.frame(test_sample$x), cl = learning_sample$y, k = 1)
+kkn_predict_y   <- as.numeric(as.character(kkn_predict))
+plot(kkn_predict_y ~ test_sample$x, las = 1)
+
+######
+# G. Recalculate the mean square error for the prediction with k nearest neighbour
+######
+
+MSE_kkn_test <-  mean((test_sample$y-kkn_predict_y)^2)
